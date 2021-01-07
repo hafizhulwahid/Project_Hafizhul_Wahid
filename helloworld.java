f5ae876340199718947ad4bc37752a8ec70cd336
isepirmansyah <77084994+isepirmansyah@users.noreply.github.com>
2021-01-07 10:41:35 +0700

    Update helloworld.java
    tanbah

--- helloworld.java
+++ helloworld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
   public static void main(){
     System.out.println("Hello Dunia Hahahaha");
     String nama = "Hafizh";
-    String alamat = "Jakarta";
+    String alamat = "Jakarta P";
     int umur = 20;
   }
 }
