f92328f54886d2d0c5d6cd3a294c074a47195808
hafizhulwahid <77084938+hafizhulwahid@users.noreply.github.com>
2021-01-07 10:40:24 +0700

    Update helloworld.java
    menambahkan String alamat

--- helloworld.java
+++ helloworld.java
@@ -2,6 +2,7 @@ public class HelloWorld {
   public static void main(){
     System.out.println("Hello Dunia Hahahaha");
     String nama = "Hafizh";
+    String alamat = "Jakarta";
     int umur = 20;
   }
 }
