a53742f448d401221560906a24093e4385c216e8
hafizhulwahid <77084938+hafizhulwahid@users.noreply.github.com>
2021-01-19 09:26:47 +0700

    Update helloworld.java
    saya membuat nama pemilik

--- helloworld.java
+++ helloworld.java
@@ -1,10 +1,11 @@
-// public class HelloWorld {
-//   public static void main(){
-//     System.out.println("Hello Dunia Hahahaha");
-//     String nama = "Hafizhul Wahid";
-//     String alamat = "Jakarta";
-//     String kelas = "PPL";
-//     int umur = 20;
+// Punya Hafizhul Wahid
+public class HelloWorld {
+  public static void main(){
+    System.out.println("Hello Dunia Hahahaha");
+    String nama = "Hafizhul Wahid";
+    String alamat = "Jakarta";
+    String kelas = "PPL";
+    int umur = 20;
     
     System.out.println("Nama saya : " + nama + ", Alamat : " + alamat + ", Umur : " + umur);
   }
