aaa921529bac14ab2067b68eb399985b9fb3ea0b
isepirmansyah <77084994+isepirmansyah@users.noreply.github.com>
2021-01-19 08:54:51 +0700

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,10 +1,10 @@
-public class HelloWorld {
-  public static void main(){
-    System.out.println("Hello Dunia Hahahaha");
-    String nama = "Hafizhul Wahid";
-    String alamat = "Jakarta";
-    String kelas = "PPL";
-    int umur = 20;
+// public class HelloWorld {
+//   public static void main(){
+//     System.out.println("Hello Dunia Hahahaha");
+//     String nama = "Hafizhul Wahid";
+//     String alamat = "Jakarta";
+//     String kelas = "PPL";
+//     int umur = 20;
     
     System.out.println("Nama saya : " + nama + ", Alamat : " + alamat + ", Umur : " + umur);
   }
