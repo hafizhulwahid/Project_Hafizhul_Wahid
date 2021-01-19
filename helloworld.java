b620e3c2a16c989cee73640508fce6f03dcb60ab
isepirmansyah <77084994+isepirmansyah@users.noreply.github.com>
2021-01-19 09:30:04 +0700

    Update helloworld.java
    Penambahan data baru

--- helloworld.java
+++ helloworld.java
@@ -1,11 +1,18 @@
-// public class HelloWorld {
-//   public static void main(){
-//     System.out.println("Hello Dunia Hahahaha");
-//     String nama = "Hafizhul Wahid";
-//     String alamat = "Jakarta";
-//     String kelas = "PPL";
-//     int umur = 20;
+public class HelloWorld {
+  public static void main(){
+    System.out.println("Hello Dunia Hahahaha");
+    String nama = "Hafizhul Wahid";
+    String alamat = "Jakarta";
+    String kelas = "PPL";
+    int umur = 20;
     
-    System.out.println("Nama saya : " + nama + ", Alamat : " + alamat + ", Umur : " + umur);
+    System.out.println("Nama saya : " + nama + ", Alamat : " + alamat + ", Umur : " + umur);  
+      
+    int angka1 = 20;
+    int angka2 = 30;
+    int jumlah = angka1 * angka2;
+    System.out.println(jumlah);
+    
+    
   }
 }
